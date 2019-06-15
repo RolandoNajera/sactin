c54101f4ed54394a31eee7ace4a09b6d6c1b3b1b
Rolando Mac <erolando.najera@gmail.com>
2019-06-15 11:13:23 -0500

    test 2 heroku

--- src/main/java/mx/globaltade/products/sactin/controllers/ProfilesController.java
+++ src/main/java/mx/globaltade/products/sactin/controllers/ProfilesController.java
@@ -24,12 +24,15 @@ public class ProfilesController {
 
     @RequestMapping(value = "/profiles", method = RequestMethod.GET)
     public String getProfiles(@RequestParam(name = "name", required = false) String name, Model model) {
-        System.out.println("llega aquí");
-        model.addAttribute("profiles", profileService.getProfiles());
-        System.out.println("llega aquí 2");
-        model.addAttribute("title", title);
-        System.out.println("llega aquí 3");
-        return "profiles/getProfiles";
+        model.addAttribute("title", "SACTIN");
+        model.addAttribute("initMessage", "Bienvenido a Sactin");
+        return "index";
+//        System.out.println("llega aquí");
+//        model.addAttribute("profiles", profileService.getProfiles());
+//        System.out.println("llega aquí 2");
+//        model.addAttribute("title", title);
+//        System.out.println("llega aquí 3");
+//        return "profiles/getProfiles";
     }
 
     @RequestMapping(value = "/profiles", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
